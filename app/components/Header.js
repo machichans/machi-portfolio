1079b7af4edd38594b32fdfff1d28a81cdc0bd3e
machichans <makoomaru@gmail.com>
2025-05-06 22:14:30 +0900

    ヘッダーをコンポーネント化 メインとフッターをコンポーネント化予定

---
+++ app/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="icon">
+                <img className="header-icon" src="image.png" alt="アイコン" />
+                <p className="icon-text">ぷちゃん　プロフィール</p>
+            </div>
+        </header>
+    )
+}
